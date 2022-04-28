0ddca8d0b969d87bf462bfc7413fb4ef9ddddacb
Henri-webdev <henri.webdev@gmail.com>
2022-04-28 14:37:47 -0500

    added full questions and answers to database and gameactivity

--- app/src/main/java/edu/lewisu/cs/yasirtahir/a3pics2tries/GameActivity.java
+++ app/src/main/java/edu/lewisu/cs/yasirtahir/a3pics2tries/GameActivity.java
@@ -25,6 +25,7 @@ import com.squareup.picasso.Picasso;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class GameActivity extends android.app.Activity{
@@ -60,7 +61,21 @@ public class GameActivity extends android.app.Activity{
                 new Question("", "",
                         "", "", ""),
                 new Question("", "",
-                        "", "", "")
+                        "", "", ""),
+                new Question("", "",
+                        "", "", ""),
+                new Question("", "",
+                        "", "", ""),
+                new Question("", "",
+                        "", "", ""),
+                new Question("", "",
+                        "", "", ""),
+                new Question("", "",
+                        "", "", ""),
+                new Question("", "",
+                        "", "", ""),
+                new Question("", "",
+                        "", "", ""),
         };
 
         // Database Portion
@@ -85,12 +100,33 @@ public class GameActivity extends android.app.Activity{
                 Picasso.get().load(questions[0].getImageId2()).into(imageView2);
                 Picasso.get().load(questions[0].getImageId3()).into(imageView3);
 
-                String imgStr4 = (String) tableImgData.get("img2");
-                String imgStr5 = (String) tableImgData.get("img3");
-                String imgStr6 = (String) tableImgData.get("img1");
-                String imgStr7 = (String) tableImgData.get("img3");
-                String imgStr8 = (String) tableImgData.get("img2");
-                String imgStr9 = (String) tableImgData.get("img1");
+                String imgStr4 = (String) tableImgData.get("img4");
+                String imgStr5 = (String) tableImgData.get("img5");
+                String imgStr6 = (String) tableImgData.get("img6");
+                String imgStr7 = (String) tableImgData.get("img7");
+                String imgStr8 = (String) tableImgData.get("img8");
+                String imgStr9 = (String) tableImgData.get("img9");
+                String imgStr10 = (String) tableImgData.get("img10");
+                String imgStr11 = (String) tableImgData.get("img11");
+                String imgStr12 = (String) tableImgData.get("img12");
+                String imgStr13 = (String) tableImgData.get("img13");
+                String imgStr14 = (String) tableImgData.get("img14");
+                String imgStr15 = (String) tableImgData.get("img15");
+                String imgStr16 = (String) tableImgData.get("img16");
+                String imgStr17 = (String) tableImgData.get("img17");
+                String imgStr18 = (String) tableImgData.get("img18");
+                String imgStr19 = (String) tableImgData.get("img19");
+                String imgStr20 = (String) tableImgData.get("img20");
+                String imgStr21 = (String) tableImgData.get("img21");
+                String imgStr22 = (String) tableImgData.get("img22");
+                String imgStr23 = (String) tableImgData.get("img23");
+                String imgStr24 = (String) tableImgData.get("img24");
+                String imgStr25 = (String) tableImgData.get("img25");
+                String imgStr26 = (String) tableImgData.get("img26");
+                String imgStr27 = (String) tableImgData.get("img27");
+                String imgStr28 = (String) tableImgData.get("img28");
+                String imgStr29 = (String) tableImgData.get("img29");
+                String imgStr30 = (String) tableImgData.get("img30");
 
                 questions[1].setImageId1(imgStr4);
                 questions[1].setImageId2(imgStr5);
@@ -98,6 +134,27 @@ public class GameActivity extends android.app.Activity{
                 questions[2].setImageId1(imgStr7);
                 questions[2].setImageId2(imgStr8);
                 questions[2].setImageId3(imgStr9);
+                questions[3].setImageId1(imgStr10);
+                questions[3].setImageId2(imgStr11);
+                questions[3].setImageId3(imgStr12);
+                questions[4].setImageId1(imgStr13);
+                questions[4].setImageId2(imgStr14);
+                questions[4].setImageId3(imgStr15);
+                questions[5].setImageId1(imgStr16);
+                questions[5].setImageId2(imgStr17);
+                questions[5].setImageId3(imgStr18);
+                questions[6].setImageId1(imgStr19);
+                questions[6].setImageId2(imgStr20);
+                questions[6].setImageId3(imgStr21);
+                questions[7].setImageId1(imgStr22);
+                questions[7].setImageId2(imgStr23);
+                questions[7].setImageId3(imgStr24);
+                questions[8].setImageId1(imgStr25);
+                questions[8].setImageId2(imgStr26);
+                questions[8].setImageId3(imgStr27);
+                questions[9].setImageId1(imgStr28);
+                questions[9].setImageId2(imgStr29);
+                questions[9].setImageId3(imgStr30);
             }
 
             @Override
@@ -122,6 +179,20 @@ public class GameActivity extends android.app.Activity{
                 questions[0].setHint(hint1);
                 questions[1].setHint(hint2);
                 questions[2].setHint(hint3);
+                String hint4 = (String) tableHintsData.get("hint4");
+                String hint5 = (String) tableHintsData.get("hint5");
+                String hint6 = (String) tableHintsData.get("hint6");
+                String hint7 = (String) tableHintsData.get("hint7");
+                String hint8 = (String) tableHintsData.get("hint8");
+                String hint9 = (String) tableHintsData.get("hint9");
+                String hint10 = (String) tableHintsData.get("hint10");
+                questions[3].setHint(hint4);
+                questions[4].setHint(hint5);
+                questions[5].setHint(hint6);
+                questions[6].setHint(hint7);
+                questions[7].setHint(hint8);
+                questions[8].setHint(hint9);
+                questions[9].setHint(hint10);
             }
 
             @Override
@@ -147,6 +218,20 @@ public class GameActivity extends android.app.Activity{
                 questions[0].setAnswer(answer1);
                 questions[1].setAnswer(answer2);
                 questions[2].setAnswer(answer3);
+                String answer4 = (String) tableAnswersData.get("answer4");
+                String answer5 = (String) tableAnswersData.get("answer5");
+                String answer6 = (String) tableAnswersData.get("answer6");
+                String answer7 = (String) tableAnswersData.get("answer7");
+                String answer8 = (String) tableAnswersData.get("answer8");
+                String answer9 = (String) tableAnswersData.get("answer9");
+                String answer10 = (String) tableAnswersData.get("answer10");
+                questions[3].setAnswer(answer4);
+                questions[4].setAnswer(answer5);
+                questions[5].setAnswer(answer6);
+                questions[6].setAnswer(answer7);
+                questions[7].setAnswer(answer8);
+                questions[8].setAnswer(answer9);
+                questions[9].setAnswer(answer10);
             }
 
             @Override
@@ -193,7 +278,7 @@ public class GameActivity extends android.app.Activity{
     }
 
     private void updateQuestion(){
-        Log.d("updateQuestion>", "ran");
+        Log.d("updateQuestion", "updated current question");
         questionTextView.setText(getResources().getString(R.string.what_word_best_describes_the_images_below));
         String hint = questions[currentIndex].getHint();
         String image1 = questions[currentIndex].getImageId1();
@@ -216,7 +301,7 @@ public class GameActivity extends android.app.Activity{
 
     private void checkAnswer(String userGuess){
         String correctAnswer = questions[currentIndex].getAnswer();
-        Log.d("check>", correctAnswer);
+        Log.d("correct answer-> ", correctAnswer);
         // Retrieve the radio button user clicked and check for right answer.
 
 
@@ -252,7 +337,7 @@ public class GameActivity extends android.app.Activity{
     private class checkButtonClickListener implements View.OnClickListener{
         @Override
         public void onClick(View view) {
-            String userGuess = userText.getText().toString();
+            String userGuess = userText.getText().toString().toLowerCase(Locale.ROOT).trim();
             checkAnswer(userGuess);
         }
     }
