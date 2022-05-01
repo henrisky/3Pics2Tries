a6dd358e1d207f96fb1b76343af6fac921c2cba9
Henri-webdev <henri.webdev@gmail.com>
2022-05-01 18:10:14 -0500

    added home button

--- app/src/main/java/edu/lewisu/cs/yasirtahir/a3pics2tries/HelpActivity.java
+++ app/src/main/java/edu/lewisu/cs/yasirtahir/a3pics2tries/HelpActivity.java
@@ -1,12 +1,22 @@
 package edu.lewisu.cs.yasirtahir.a3pics2tries;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HelpActivity extends android.app.Activity{
+    Button homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(view -> {
+            Intent returnIntent = new Intent();
+            setResult(RESULT_OK, returnIntent);
+            finish();
+        });
     }
 }
